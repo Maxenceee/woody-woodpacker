e8dabb52ae0ae077ccefd753ca66561edcf44dc3
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-04-29 11:37:29 +0200

    Update ttest.c

--- ttest.c
+++ ttest.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
-int square(int x)
+int main(int argc, char const *argv[])
 {
-    return x*x;
-}
-
-int main(int argc, char** argv)
-{
-    fprintf(stdout, "Hello world!\n");
-    fprintf(stdout, "fprintf=%p, stdout=%p\n", fprintf, stdout);
-    fprintf(stdout, "square(10) = %d\n", square(10));
-
-    return 0;
+	printf("Hello, World!\n");
+	return 0;
 }
