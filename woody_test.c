fb8019c6c685da8e8cf8d02c6f3f419e20957a9c
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-04-29 10:53:21 +0200

    up

---
+++ woody_test.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+    // Ouvrir le fichier ELF chiffré en lecture
+    int fd = open("fichier", O_RDONLY);
+    if (fd == -1) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return 1;
+    }
+
+    // Obtenir la taille du fichier
+    off_t taille_fichier = lseek(fd, 0, SEEK_END);
+    if (taille_fichier == -1) {
+        perror("Erreur lors de la détermination de la taille du fichier");
+        close(fd);
+        return 1;
+    }
+
+    // Revenir au début du fichier
+    if (lseek(fd, 0, SEEK_SET) == -1) {
+        perror("Erreur lors du retour au début du fichier");
+        close(fd);
+        return 1;
+    }
+
+    // Allouer de la mémoire pour stocker les données du fichier
+    void *donnees_chiffrees = malloc(taille_fichier);
+    if (!donnees_chiffrees) {
+        perror("Erreur lors de l'allocation de mémoire pour les données chiffrées");
+        close(fd);
+        return 1;
+    }
+
+    // Lire les données du fichier
+    ssize_t bytes_lus = read(fd, donnees_chiffrees, taille_fichier);
+    if (bytes_lus != taille_fichier) {
+        perror("Erreur lors de la lecture du fichier");
+        close(fd);
+        free(donnees_chiffrees);
+        return 1;
+    }
+
+    // Fermer le fichier
+    close(fd);
+
+    // Charger les données déchiffrées en mémoire avec autorisations d'exécution
+    void *adresse_memoire = mmap(NULL, taille_fichier, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (adresse_memoire == MAP_FAILED) {
+        perror("Erreur lors du chargement des données en mémoire");
+        free(donnees_chiffrees);
+        return 1;
+    }
+
+    // Copier les données déchiffrées dans la mémoire allouée
+    memcpy(adresse_memoire, donnees_chiffrees, taille_fichier);
+
+	// Autoriser l'exécution du code en mémoire
+    if (mprotect(adresse_memoire, taille_fichier, PROT_READ | PROT_EXEC) == -1) {
+        perror("Erreur lors de la modification des autorisations de mémoire");
+        munmap(adresse_memoire, taille_fichier);
+        free(donnees_chiffrees);
+        return 1;
+    }
+
+    // Exécuter le code en lançant le pointeur vers le point d'entrée du programme
+    void (*entry_point)() = (void (*)())adresse_memoire;
+	printf("%p\n", entry_point);
+	printf("...Woody...\n");
+    entry_point();
+
+    // Libérer la mémoire utilisée
+    munmap(adresse_memoire, taille_fichier);
+    free(donnees_chiffrees);
+
+    return 0;
+}
