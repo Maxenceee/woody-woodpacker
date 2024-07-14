f07960f5ec27fcdd70272437d5bf65afe4f94753
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-07-14 11:29:51 +0200

    pa

--- sources/payload/payload_64.asm
+++ sources/payload/payload_64.asm
@@ -5,43 +5,42 @@ global _payload_64
 segment .text align=16
 
 _payload_64:
-  push rax
-  push rdi
-  push rsi
-  push rsp
-  push rdx
-  push rcx
-  push rbx
-  call get_my_loc
-    sub edx, next_i - msg
-    mov ecx, edx
-    mov edx, msg_len
-    mov ebx, 1
-    mov eax, 4
-    syscall
+	push rax
+	push rdi
+	push rsi
+	push rsp
+	push rdx
+	push rcx
+	push rbx
+	call get_my_loc
+	sub edx, next_i - msg
+	mov ecx, edx
+	mov edx, msg_len
+	mov ebx, 1
+	mov eax, 4
+	syscall
 
 get_my_loc:
-    call next_i
+	call next_i
 
 next_i:
-    pop rdx
-    ret
+	pop rdx
+	ret
 
 msg	db "....WOODY....", 0x0a, 0
 msg_len	equ	$ - msg
 
 clean:
-    ; Reset the stack
-    add rsp, 160
-  pop rbx
-  pop rcx
-  pop rdx
-  pop rsp
-  pop rsi
-  pop rdi
-  pop rax
-    ; jmp	0xFFFFFFFF
-  ret
+	; Reset the stack
+	add rsp, 160
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rsp
+	pop rsi
+	pop rdi
+	pop rax
+	jmp	0xFFFFFFFF
 
 info_start:
 key:					dq	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
