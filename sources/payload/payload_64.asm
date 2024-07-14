4b3e39b8ee3add00a49e50bf10bf362204ed5cf1
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-07-14 18:08:05 +0200

    up

--- sources/payload/payload_64.asm
+++ sources/payload/payload_64.asm
@@ -1,32 +1,36 @@
 global _payload_64
 
-[BITS 32]
+[BITS 64]
 
 segment .text align=16
 
 _payload_64:
-; 	push rax
-; 	push rdx
-; 	push rsi
-; 	push rdi
-; 	jmp .print_start_msg
-; .displayed_str:
-; 	db "....WOODY....", 0x0a, 0
-; .print_start_msg:
-; 	mov rax, 0x1
-; 	mov rdi, 1
-; 	lea rsi, [rel .displayed_str]
-; 	mov rdx, 15
-; 	syscall
+	push rbp
+	mov rbp, rsp
+	push rax
+	push rdx
+	push rsi
+	push rdi
+	jmp .print_start_msg
+.displayed_str:
+	db "....WOODY....", 0x0a, 0
+.print_start_msg:
+	mov rax, 0x1
+	mov rdi, 1
+	lea rsi, [rel .displayed_str]
+	mov rdx, 15
+	syscall
 
-; 	pop rdi
-; 	pop rsi
-; 	pop rdx
-; 	pop rax
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rax
+	mov rsp, rbp
+	pop rbp
 	jmp	0x01020304
 
-; info_start:
-; key:					dq	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-; encrypted_data_start:	dq  0xbbbbbbbbbbbbbbbb
-; encrypted_data_len:		dq	0xcccccccccccccccc
-; start_encode:			dq  0xdddddddddddddddd
+info_start:
+key:					dq	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+encrypted_data_start:	dq  0xbbbbbbbbbbbbbbbb
+encrypted_data_len:		dq	0xcccccccccccccccc
+start_encode:			dq  0xdddddddddddddddd
