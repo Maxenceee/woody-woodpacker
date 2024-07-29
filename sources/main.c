5294f33dfc1b8103e82ed1163841dd95d3608d9b
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-07-30 01:27:18 +0200

    Update main.c

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 14:07:36 by mbrement          #+#    #+#             */
-/*   Updated: 2024/07/29 11:50:50 by mgama            ###   ########.fr       */
+/*   Updated: 2024/07/30 01:10:11 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -219,7 +219,12 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	
-	packer(elf_file);
+	if (packer(elf_file) == -1)
+	{
+		delete_elf_file(elf_file);
+		ft_error("An error occured during the packing process");
+		return (1);
+	}
 	ft_verbose(B_GREEN"\nFile `%s%s%s` successfully packed in %s%s%s !\n"RESET, B_CYAN, target, B_GREEN, B_CYAN, WD_OUTPUT_FILE, B_GREEN);
 	delete_elf_file(elf_file);
 }
