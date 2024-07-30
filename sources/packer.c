1d8b37a566f8586a79055d78a8bf0874f867c088
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-07-30 12:47:42 +0200

    Update packer.c

--- sources/packer.c
+++ sources/packer.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:59:30 by mgama             #+#    #+#             */
-/*   Updated: 2024/07/30 12:46:26 by mgama            ###   ########.fr       */
+/*   Updated: 2024/07/30 12:46:42 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,9 +46,7 @@ t_elf_section	*get_text_section(t_elf_file *elf)
 int	encrypt_text_section(t_elf_file *elf)
 {
 	ft_verbose("\nEncrypting the `%s%s%s` section...\n", B_CYAN, ".text", RESET);
-	// t_elf_section	*text_section = get_text_section(elf);
-	(void)elf;
-	t_elf_section *text_section = NULL;
+	t_elf_section	*text_section = get_text_section(elf);
 	if (text_section == NULL)
 		return (ft_error("The text section could not be found in the file"), -1);
 
