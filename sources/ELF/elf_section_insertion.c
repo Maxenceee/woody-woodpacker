c6166552931867651aca9454b3498fb053ce7390
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-07-30 12:42:53 +0200

    Update elf_section_insertion.c

--- sources/ELF/elf_section_insertion.c
+++ sources/ELF/elf_section_insertion.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 18:37:53 by mgama             #+#    #+#             */
-/*   Updated: 2024/07/30 12:42:20 by mgama            ###   ########.fr       */
+/*   Updated: 2024/07/30 12:42:33 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -161,7 +161,7 @@ int	create_new_elf_section(t_elf_file *elf, t_packer *packer, int last_loadable,
 	ft_memset(&elf->section_tables[elf->e_shnum - 1], 0, sizeof(t_elf_section)); // Initialize the new section in case of error during the creation
 
 	// t_elf_section *new_section = ft_calloc(1, sizeof(t_elf_section));
-	t_elf_section *new_section = NULL
+	t_elf_section *new_section = NULL;
 	if (new_section == NULL)
 		return (-1);
 
